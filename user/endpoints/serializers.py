b3a4846fbcf28ca0db88597e06d9874767a1e358
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-04-22 16:31:36 -0500

    Master:<login/>: endpoint listo, a probar la integration con el frontend.

--- user/endpoints/serializers.py
+++ user/endpoints/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from django.db.models import Q
 from user.models import UserModel
 
@@ -13,8 +14,12 @@ class LoginSerializer(serializers.Serializer):
   password = serializers.CharField(required=True)
 
   def validate(self, data):
-    query = Q(email=data['email']) | Q(username=data['username'])
-    user = UserModel.objects.filter(query, password=data['password'])
-    import pdb; pdb.set_trace()
-
-
+    uoe = data['username_or_email']
+    self.user = UserModel.objects.filter(
+      Q(email=uoe) | Q(username=uoe), password=data['password'])
+    if not self.user.exists():
+      raise ValidationError({ 'user': 'Invalid username/email or password.'})
+    return data
+  
+  def create(self, validated_data):
+    return self.user.first()
--- user/endpoints/viewsets.py
+++ user/endpoints/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK
 from rest_framework.decorators import action
 from .serializers import LogupSerializer, LoginSerializer, UserModel
 from django.forms.models import model_to_dict
@@ -14,16 +14,20 @@ class UserViewset(GenericViewSet):
     path = self.request.get_full_path()
     return LogupSerializer if 'logup' in path else LoginSerializer
 
+  def user_response(self, user, status):
+    data = model_to_dict(user, ['username', 'email', 'hash'])
+    return Response(
+      data={ 'user': data, 'status': status },
+      status=HTTP_201_CREATED if status == 'CREATED' else HTTP_200_OK)
+
   @action(detail=False, methods=['post'])
   def logup(self, request):
-    self.get_serializer_class()
-    serializer = self.serializer_class(data=request.data)
+    serializer = self.get_serializer_class()(data=request.data)
     serializer.is_valid(raise_exception=True)
-    user = serializer.save()
-    return Response(data={
-      'user': model_to_dict(user, ['username', 'email', 'hash']),
-      'status': 'CREATED', }, status=HTTP_201_CREATED)
+    return user_response(serializer.save(), 'CREATED')
   
   @action(detail=False, methods=['post'])
   def login(self, request):
-    import pdb; pdb.set_trace()
+    serializer = self.get_serializer_class()(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    return user_response(serializer.save(), 'VALIDATED')
