562fc1a680af2e245af56b0a42bab77557c97993
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-04-22 15:36:16 -0500

    Master:<login/>: agregado el endpoint falta validar y probar.

--- user/endpoints/serializers.py
+++ user/endpoints/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
+from django.db.models import Q
 from user.models import UserModel
 
 class LogupSerializer(serializers.ModelSerializer):
   class Meta:
     model = UserModel
     exclude = ['hash']
+  
+class LoginSerializer(serializers.Serializer):
+  
+  username_or_email = serializers.CharField(required=True)
+  password = serializers.CharField(required=True)
+
+  def validate(self, data):
+    query = Q(email=data['email']) | Q(username=data['username'])
+    user = UserModel.objects.filter(query, password=data['password'])
+    import pdb; pdb.set_trace()
+
 
--- user/endpoints/viewsets.py
+++ user/endpoints/viewsets.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.decorators import action
-from .serializers import LogupSerializer, UserModel
+from .serializers import LogupSerializer, LoginSerializer, UserModel
 from django.forms.models import model_to_dict
 
 class UserViewset(GenericViewSet):
@@ -10,11 +10,20 @@ class UserViewset(GenericViewSet):
   serializer_class = LogupSerializer
   queryset = UserModel.objects.all()
 
+  def get_serializer_class(self):
+    path = self.request.get_full_path()
+    return LogupSerializer if 'logup' in path else LoginSerializer
+
   @action(detail=False, methods=['post'])
   def logup(self, request):
+    self.get_serializer_class()
     serializer = self.serializer_class(data=request.data)
     serializer.is_valid(raise_exception=True)
     user = serializer.save()
     return Response(data={
       'user': model_to_dict(user, ['username', 'email', 'hash']),
       'status': 'CREATED', }, status=HTTP_201_CREATED)
+  
+  @action(detail=False, methods=['post'])
+  def login(self, request):
+    import pdb; pdb.set_trace()
